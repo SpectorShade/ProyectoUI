f566335c86e882f81ba4bb8abde7f8064123407f
SpectorShade <92053721+SpectorShade@users.noreply.github.com>
2025-09-18 09:56:05 -0600

    Update SeccionBotones.swift

--- Proyecto_UI/Proyecto_UI/ui/secciones/SeccionBotones.swift
+++ Proyecto_UI/Proyecto_UI/ui/secciones/SeccionBotones.swift
@@ -10,22 +10,10 @@ import SwiftUI
 struct SeccionBotones: View {
     var body: some View {
         VStack(alignment: .leading) {
-            Text("BUTTONS")
-                .font(.headline)
-                .foregroundColor(.white)
-        
-            HStack {
-                Circle().fill(Color.green).frame(width: 10, height: 10)
-                Text("SELECT").foregroundColor(.white)
-            }
-            HStack {
-                Circle().fill(Color.red).frame(width: 10, height: 10)
-                Text("BACK").foregroundColor(.white)
-            }
-            HStack {
-                Circle().fill(Color.yellow).frame(width: 10, height: 10)
-                Text("HELP").foregroundColor(.white)
-            }
+             TituloParaSeccion(texto: "BUTTONS")
+             IndicadorParaBoton(color: .green, etiqueta: "Select")
+             IndicadorParaBoton(color: .red, etiqueta: "Back")
+             IndicadorParaBoton(color: .yellow, etiqueta: "Help")
         }
         .padding()
     }
