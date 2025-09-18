87d7af9a8356adb0dcb6b307581ef3d0858831bb
SpectorShade <92053721+SpectorShade@users.noreply.github.com>
2025-09-18 09:41:48 -0600

    Create FilaParaEstadistica.swift

---
+++ Proyecto_UI/Proyecto_UI/ui/componentes/FilaParaEstadistica.swift
@@ -0,0 +1,27 @@
+//
+//  FilaParaEstadistica.swift
+//  Proyecto_UI
+//
+//  Created by alumno on 18/09/25.
+//
+
+import SwiftUI
+
+struct FilaParaEstadistica: View {
+    var etiqueta: String
+    var valor: String
+    
+    var body: some View {
+        HStack {
+            Text(etiqueta)
+                .foregroundColor(.gray)
+            Spacer()
+            Text(valor)
+                .foregroundColor(.white)
+        }
+    }
+}
+
+#Preview {
+    StatRow(etiqueta: "HEALTH", valor: "85%")
+}
